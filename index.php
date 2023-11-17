8997871ee076578152a78b7c3c029f4063dfbc31
Ale Raf <raffinalexis@gmail.com>
2023-11-17 17:51:54 +0100

    including every file to index

--- index.php
+++ index.php
@@ -1,37 +1,11 @@
-<?php
-require_once "config.php";
-
-$sql = "SELECT * FROM `works`";
-$req = $db->query($sql);
-$articles = $req->fetchAll();
-
-include "header.php";
-
-// $to = 'nobody@example.com';
-// $subject = 'the subject';
-// $message = 'hello';
-// mail($to, $subject, $message) 
-?>
-
+<?php include "header.php" ?>
 
 <main>
     <?php include "hero.php" ?>
     
     <?php include "skills.php" ?>
     
-    <section id="works">
-        <h1>Projets</h1>
-        <?php foreach($articles as $article): ?>
-            <article>
-                <h1>
-                    <a href="projet.php?id=<?= $article["id"] ?>">
-                        <?= $article["title"] ?>
-                    </a>
-                </h1>
-                <p><?= $article["subtitle"] ?></p>
-            </article>
-        <?php endforeach; ?>
-    </section>
+    <?php include "works.php" ?>
 </main>
 
 <?php include "footer.php" ?>
